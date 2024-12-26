482a4ba48aea430f7b68f00f1439d95036f4d305
FaridRodriguez <faridrodriguez.dev@gmail.com>
2024-12-26 06:24:29 +0000

    CHORE make funciton names more readable

--- src/data/sampling.py
+++ src/data/sampling.py
@@ -10,7 +10,7 @@ def bootstrap_replicate(data, func):
     return func(bs_sample)
 
 
-def draw_bs_reps(data, func, size=1):
+def draw_bootsrap_replicates(data, func, size=1):
     """Draw bootstrap replicates."""
     bs_replicates = np.empty(size)
     for i in range(size):
@@ -18,7 +18,7 @@ def draw_bs_reps(data, func, size=1):
     return bs_replicates
 
 
-def draw_bs_pairs_linreg(x, y, size=1):
+def draw_bootrap_pairs_linreg(x, y, size=1):
     """Perform pairs bootstrap for linear regression."""
     inds = np.arange(start=0, stop=len(x), step=1)
     bs_slope_reps = np.empty(size)
@@ -37,6 +37,15 @@ def permutation_sample(data1, data2):
     perm_sample_1 = permuted_data[:len(data1)]
     perm_sample_2 = permuted_data[len(data1):]
     return perm_sample_1, perm_sample_2
+
+
+def draw_permutation_replicates(data_1, data_2, func, size=1):
+    """Generate multiple permutation replicates."""
+    perm_replicates = np.empty(size)
+    for i in range(size):
+        perm_sample_1, perm_sample_2 = permutation_sample(data_1, data_2)
+        perm_replicates[i] = func(perm_sample_1, perm_sample_2)
+    return perm_replicates
 
 
 def plot_permutation_ecdfs(data1, data2, n_samples=50):
