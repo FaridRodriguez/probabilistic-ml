227fc63a81275f466885c6dd7b3788f1c49efce3
FaridRodriguez <faridrodriguez.dev@gmail.com>
2024-12-27 21:48:24 +0000

    FEAT include correlation coefficient function

--- src/common/eda_utils.py
+++ src/common/eda_utils.py
@@ -6,3 +6,9 @@ def ecdf(data):
     x = np.sort(data)
     y = [i / n for i in range(1, n + 1)]
     return x, y
+
+
+def pearson_corr_coef(x, y):
+    """Compute Pearson correlation coefficient between two arrays."""
+    corr_mat = np.corrcoef(x, y)
+    return corr_mat[0, 1]
